f7a232683c3457355a16acb07d773a8c0e9eff37
Jhionan <jhionan@gmail.com>
2023-02-23 01:41:11 -0300

    refactor(constants): rename to images

---
+++ lib/core/images.dart
@@ -0,0 +1,32 @@
+abstract class Images {
+  static const String brushingTeeth = 'assets/actions/brushing_teeth.webp';
+  static const String brushingTeeth1 = 'assets/actions/brushing_teeth1.webp';
+  static const String brushingTeeth2 = 'assets/actions/brushing_teeth2.webp';
+  static const String combingHair = 'assets/actions/combing_hair.webp';
+  static const String combingHair1 = 'assets/actions/combing_hair1.webp';
+  static const String combingHair2 = 'assets/actions/combing_hair2.webp';
+  static const String combingHair3 = 'assets/actions/combing_hair3.webp';
+  static const String drinkingWater = 'assets/actions/drinking_water.webp';
+  static const String drinkingWater1 = 'assets/actions/drinking_water1.webp';
+  static const String drinkingWater2 = 'assets/actions/drinking_water2.webp';
+  static const String eating = 'assets/actions/eating.webp';
+  static const String eating1 = 'assets/actions/eating1.webp';
+  static const String eating2 = 'assets/actions/eating2.webp';
+  static const String laughing = 'assets/actions/laughing.webp';
+  static const String laughing2 = 'assets/actions/laughing2.webp';
+  static const String reading = 'assets/actions/reading.webp';
+  static const String reading1 = 'assets/actions/reading1.webp';
+  static const String reading2 = 'assets/actions/reading2.webp';
+  static const String running = 'assets/actions/running.webp';
+  static const String running1 = 'assets/actions/running1.webp';
+  static const String sleeping = 'assets/actions/sleeping.webp';
+  static const String sleeping1 = 'assets/actions/sleeping1.webp';
+  static const String takingShower = 'assets/actions/taking_shower.webp';
+  static const String takingShower1 = 'assets/actions/taking_shower1.webp';
+  static const String takingShower2 = 'assets/actions/taking_shower2.webp';
+  static const String washingHands = 'assets/actions/washing_hands.webp';
+  static const String washingHands1 = 'assets/actions/washing_hands1.webp';
+  static const String washingHands2 = 'assets/actions/washing_hands2.webp';
+  static const String watchingTv = 'assets/actions/watching_tv.webp';
+  static const String watchingTv1 = 'assets/actions/watching_tv1.webp';
+}
