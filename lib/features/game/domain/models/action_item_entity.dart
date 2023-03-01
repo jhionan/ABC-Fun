02b118c4c079b6a1384a4275c93e0860a98152e4
Jhionan <jhionan@gmail.com>
2023-03-01 12:51:12 -0300

    int: add more intl strings

--- lib/features/game/domain/models/action_item_entity.dart
+++ lib/features/game/domain/models/action_item_entity.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 class ActionItemEntity {
   final ActionGroup group;
   final String name;
@@ -43,4 +46,33 @@ enum ActionGroup {
   takingShower,
   washingHands,
   watchingTv,
+}
+
+extension ActionGroupNameEx on ActionGroup {
+  String actionName(BuildContext context) {
+    switch(this) {
+      case ActionGroup.brushingTeeth:
+        return AppLocalizations.of(context)!.brushingTeeth;
+      case ActionGroup.combingHair:
+        return AppLocalizations.of(context)!.combingHair;
+      case ActionGroup.drinkingWater:
+        return AppLocalizations.of(context)!.drinkingWater;
+      case ActionGroup.eating:
+        return AppLocalizations.of(context)!.eating;
+      case ActionGroup.laughing:
+        return AppLocalizations.of(context)!.laughing;
+      case ActionGroup.reading:
+        return AppLocalizations.of(context)!.reading;
+      case ActionGroup.running:
+        return AppLocalizations.of(context)!.running;
+      case ActionGroup.sleeping:
+        return AppLocalizations.of(context)!.sleeping;
+      case ActionGroup.takingShower:
+        return AppLocalizations.of(context)!.takingShower;
+      case ActionGroup.washingHands:
+        return AppLocalizations.of(context)!.washingHands;
+      case ActionGroup.watchingTv:
+        return AppLocalizations.of(context)!.watchingTv;
+    }
+  }
 }
