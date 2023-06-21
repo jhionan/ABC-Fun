021a09d5c6e77041c30d0c90515564960e6afd4a
Jhionan <jhionan@gmail.com>
2023-06-20 22:19:45 -0300

    feat(Navigation): add navigation setup

--- lib/core/navigation/abc_router.dart
+++ lib/core/navigation/abc_router.dart
@@ -1,18 +1,34 @@
 import 'package:aba/core/navigation/abc_router.gr.dart';
+
 import 'package:auto_route/auto_route.dart';
 
-
-
-@AutoRouterConfig()
+@AutoRouterConfig(
+  replaceInRouteName: 'Provider,Route',
+)
 class AbcRouter extends $AbcRouter {
-
-@override      
-RouteType get defaultRouteType => const RouteType.material();
+  @override
+  RouteType get defaultRouteType => const RouteType.adaptive();
 
   @override
   List<AutoRoute> get routes => [
-
-        AutoRoute(initial: true, page: Dashboard.page, path: '/',),
-        AutoRoute(page: GameProvider.page, path: '/game')
+        AutoRoute(
+          page: MenuRoute.page,
+          initial: true,
+          path: '/${AbcRoute.menu.pathFragment}',
+          title: (context, data) => 'Menu',
+          children: [
+            AutoRoute(page: Dashboard.page, path: AbcRoute.dashboard.pathFragment, initial: true),
+          ],
+        ),
+        AutoRoute(page: GameRoute.page, path: '/${AbcRoute.game.pathFragment}')
       ];
+}
+
+enum AbcRoute {
+  menu('menu'),
+  dashboard('dashboard'),
+  game('game');
+
+  const AbcRoute(this.pathFragment);
+  final String pathFragment;
 }
--- lib/core/navigation/abc_router.gr.dart
+++ lib/core/navigation/abc_router.gr.dart
@@ -10,23 +10,30 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:aba/features/dashboard/presentation/view/dashboard.dart' as _i2;
 import 'package:aba/features/game/presentation/view/game_provider.dart' as _i1;
-import 'package:auto_route/auto_route.dart' as _i3;
+import 'package:aba/features/menu/bloc/menu_provider.dart' as _i3;
+import 'package:auto_route/auto_route.dart' as _i4;
 
-abstract class $AbcRouter extends _i3.RootStackRouter {
+abstract class $AbcRouter extends _i4.RootStackRouter {
   $AbcRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i3.PageFactory> pagesMap = {
-    GameProvider.name: (routeData) {
-      return _i3.AutoRoutePage<dynamic>(
+  final Map<String, _i4.PageFactory> pagesMap = {
+    GameRoute.name: (routeData) {
+      return _i4.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i1.GameProvider(),
       );
     },
     Dashboard.name: (routeData) {
-      return _i3.AutoRoutePage<dynamic>(
+      return _i4.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i2.Dashboard(),
+      );
+    },
+    MenuRoute.name: (routeData) {
+      return _i4.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i3.MenuProvider(),
       );
     },
   };
@@ -34,22 +41,22 @@ abstract class $AbcRouter extends _i3.RootStackRouter {
 
 /// generated route for
 /// [_i1.GameProvider]
-class GameProvider extends _i3.PageRouteInfo<void> {
-  const GameProvider({List<_i3.PageRouteInfo>? children})
+class GameRoute extends _i4.PageRouteInfo<void> {
+  const GameRoute({List<_i4.PageRouteInfo>? children})
       : super(
-          GameProvider.name,
+          GameRoute.name,
           initialChildren: children,
         );
 
-  static const String name = 'GameProvider';
+  static const String name = 'GameRoute';
 
-  static const _i3.PageInfo<void> page = _i3.PageInfo<void>(name);
+  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
 }
 
 /// generated route for
 /// [_i2.Dashboard]
-class Dashboard extends _i3.PageRouteInfo<void> {
-  const Dashboard({List<_i3.PageRouteInfo>? children})
+class Dashboard extends _i4.PageRouteInfo<void> {
+  const Dashboard({List<_i4.PageRouteInfo>? children})
       : super(
           Dashboard.name,
           initialChildren: children,
@@ -57,5 +64,19 @@ class Dashboard extends _i3.PageRouteInfo<void> {
 
   static const String name = 'Dashboard';
 
-  static const _i3.PageInfo<void> page = _i3.PageInfo<void>(name);
+  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i3.MenuProvider]
+class MenuRoute extends _i4.PageRouteInfo<void> {
+  const MenuRoute({List<_i4.PageRouteInfo>? children})
+      : super(
+          MenuRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'MenuRoute';
+
+  static const _i4.PageInfo<void> page = _i4.PageInfo<void>(name);
 }
