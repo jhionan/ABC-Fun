f612083f231e306d6eec7aaff0c8b96b6c835fdb
Jhionan <jhionan@gmail.com>
2023-06-25 22:52:13 -0300

    refactor: update dashboard widgets

--- lib/features/dashboard/presentation/view/widgets/report_widget.dart
+++ lib/features/dashboard/presentation/view/widgets/report_widget.dart
@@ -2,7 +2,7 @@ import 'package:aba/core/images.dart';
 import 'package:aba/core/theme/dimensions.dart';
 import 'package:aba/core/utils/extensions/context_ext.dart';
 import 'package:aba/features/dashboard/presentation/view/widgets/report_card.dart';
-import 'package:aba/features/dashboard/presentation/view/widgets/report_result_widget_title.dart';
+import 'package:aba/core/utils/widgets/abc_title_widget.dart';
 import 'package:aba/features/game/domain/models/action_item_entity.dart';
 import 'package:aba/features/game/domain/models/action_statistics.dart';
 import 'package:flutter/widgets.dart';
@@ -44,7 +44,7 @@ class ReportWidget extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ReportResultWidgetTitle(imageUrl: Images.performanceIcon, title: context.intl.reportWidgetByTheme),
+            AbcTitleWidget.small(imageUrl: Images.performanceIcon, title: context.intl.reportWidgetByTheme),
             ...actionStatistics
                 .map((e) => Padding(
                       padding: EdgeInsets.only(top: context.dimensions.vMargin),
--- lib/features/dashboard/presentation/view/widgets/results_widget.dart
+++ lib/features/dashboard/presentation/view/widgets/results_widget.dart
@@ -1,22 +1,18 @@
 import 'package:aba/core/images.dart';
 import 'package:aba/core/theme/dimensions.dart';
 import 'package:aba/core/utils/extensions/context_ext.dart';
-import 'package:aba/features/dashboard/presentation/view/widgets/report_result_widget_title.dart';
+import 'package:aba/core/utils/widgets/abc_title_widget.dart';
 import 'package:aba/features/dashboard/presentation/view/widgets/result_card.dart';
 import 'package:aba/features/game/domain/models/action_statistics.dart';
 import 'package:flutter/material.dart';
 
 class ResultsWidget extends StatelessWidget {
   const ResultsWidget({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
-    TotalActionResults totalActionResults = TotalActionResults(
-    averangeCorrect: 50,
-    totalShown: 15,
-    completedRounds: 10
-  );
+    TotalActionResults totalActionResults =
+        TotalActionResults(averangeCorrect: 50, totalShown: 15, completedRounds: 10);
     return ConstrainedBox(
       constraints: BoxConstraints(
         maxWidth: context.dimensions.maxHorizontalWidth,
@@ -25,35 +21,35 @@ class ResultsWidget extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: context.dimensions.hMargin, vertical: context.dimensions.vMargin),
         child: Column(
           children: [
-           ReportResultWidgetTitle(
-            imageUrl: Images.supportIcon,
-            title: context.intl.resultWidget,
-           ),
-           Padding(
-             padding: EdgeInsets.only(top: context.dimensions.vMargin),
-             child: ResultCard(
-              imageUrl: Images.averageResultsIcon,
-              text: context.intl.resultCardAverange,
-              result: totalActionResults.averangeCorrect,
-              type: ResultCardType.large,
-             ),
-           ),
-           Padding(
-             padding: EdgeInsets.only(top: context.dimensions.vMargin),
-             child: ResultCard(
-              imageUrl: Images.totalChallengesResults,
-              text: context.intl.resultCardDoneChallanges,
-              result: totalActionResults.totalShown,
-             ),
-           ),
-           Padding(
-             padding: EdgeInsets.only(top: context.dimensions.vMargin),
-             child: ResultCard(
-              imageUrl: Images.totalAverangeSuccessResults,
-              text: context.intl.resultCardSucessChallenges,
-              result: totalActionResults.completedRounds,
-             ),
-           ),
+            AbcTitleWidget.small(
+              imageUrl: Images.supportIcon,
+              title: context.intl.resultWidget,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: context.dimensions.vMargin),
+              child: ResultCard(
+                imageUrl: Images.averageResultsIcon,
+                text: context.intl.resultCardAverange,
+                result: totalActionResults.averangeCorrect,
+                type: ResultCardType.large,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: context.dimensions.vMargin),
+              child: ResultCard(
+                imageUrl: Images.totalChallengesResults,
+                text: context.intl.resultCardDoneChallanges,
+                result: totalActionResults.totalShown,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: context.dimensions.vMargin),
+              child: ResultCard(
+                imageUrl: Images.totalAverangeSuccessResults,
+                text: context.intl.resultCardSucessChallenges,
+                result: totalActionResults.completedRounds,
+              ),
+            ),
           ],
         ),
       ),
