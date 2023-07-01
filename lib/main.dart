991b770b41ad682b81ae98b59ec9d0baec04cbf0
Jhionan <jhionan@gmail.com>
2023-07-01 00:34:38 -0300

    feat: add firebase setup for ios, macos, linux, windows web

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,36 @@ import 'package:aba/core/providers/providers.dart';
 import 'package:aba/core/theme/abc_fun_theme.dart';
 import 'package:aba/core/theme/dimensions.dart';
 import 'package:aba/core/utils/widgets/adaptative_screen_builder.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   provider = ProviderContainer();
   setPathUrlStrategy();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const MyApp());
+
+  
 }
 
 class MyApp extends StatelessWidget {
@@ -24,28 +44,29 @@ class MyApp extends StatelessWidget {
     final appRouter = provider.read<AbcRouter>(Providers.appRouter);
     return AdaptativeScreenBuilder(builder: (context, type) {
       return DimensionsProvider(
-          dimensions: switch (type) {
-            AdaptativeScreenType.handset => Dimensions.handSet(),
-            _ => Dimensions.biggerScreens(),
-          },
-          child: MaterialApp.router(
-            routerConfig: appRouter.config(),
-            title: 'ABC fun',
-            theme: AbcTheme().lightTheme(),
-            localizationsDelegates: const [
-              AppLocalizations.delegate, // Add this line
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: const [
-              Locale('pt', 'BR'),
-              Locale('en', 'US'),
-              Locale('fr', 'FR'),
-              Locale('es', 'ES'),
-              Locale('de', 'DE'),
-            ],
-          ));
+        dimensions: switch (type) {
+          AdaptativeScreenType.handset => Dimensions.handSet(),
+          _ => Dimensions.biggerScreens(),
+        },
+        child: MaterialApp.router(
+          routerConfig: appRouter.config(),
+          title: 'ABC fun',
+          theme: AbcTheme().lightTheme(),
+          localizationsDelegates: const [
+            AppLocalizations.delegate, // Add this line
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('pt', 'BR'),
+            Locale('en', 'US'),
+            Locale('fr', 'FR'),
+            Locale('es', 'ES'),
+            Locale('de', 'DE'),
+          ],
+        ),
+      );
     });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,23 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import cloud_firestore
+import firebase_analytics
+import firebase_auth
+import firebase_core
+import firebase_crashlytics
 import isar_flutter_libs
 import path_provider_foundation
+import record_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
 }
