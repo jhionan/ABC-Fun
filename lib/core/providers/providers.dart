cf4e86e96d2180543ded6e5b24346b5e3ca789e3
Jhionan <jhionan@gmail.com>
2023-07-01 15:42:31 -0300

    refactor: remove audio player from app level

--- lib/core/providers/providers.dart
+++ lib/core/providers/providers.dart
@@ -2,7 +2,6 @@ import 'package:aba/core/actions_audio.dart';
 import 'package:aba/core/db/schemas/action_custom_item_entity.dart';
 import 'package:aba/core/navigation/abc_router.dart';
 import 'package:aba/core/utils/helpers/path_provider_helper/path_provider_helper.dart';
-import 'package:audioplayers/audioplayers.dart';
 import 'package:isar/isar.dart';
 import 'package:riverpod/riverpod.dart';
 
@@ -17,16 +16,17 @@ final class Providers {
     return ActionAudio();
   });
 
-  static final audioPlayer = Provider<AudioPlayer>((ref) {
-    return AudioPlayer();
+  static final pathProviderHelper = Provider<PathProviderHelper>((ref) {
+    return PathProviderHelper();
   });
 
   static final db = Provider<Future<Isar>>((ref) async {
-    final path = await  PathProviderHelper().getApplicationDocumentsDirectoryPath();
+    final path = await   ref.read<PathProviderHelper>(pathProviderHelper).getApplicationDocumentsDirectoryPath();
     final isar = await Isar.open(
       [ActionCustomItemEntitySchema],
       directory: path,
     );
     return isar;
   });
+
 }
