296835c0ed4b9e057da8f79889862cca8ca86fac
Jhionan <jhionan@gmail.com>
2023-07-02 23:38:40 -0300

    feat: abc drop down - put abc style ;)

--- lib/core/domain/view/widgets/abc_drop_down.dart
+++ lib/core/domain/view/widgets/abc_drop_down.dart
@@ -1,3 +1,5 @@
+import 'package:abc_fun/core/theme/abc_colors.dart';
+import 'package:abc_fun/core/theme/dimensions.dart';
 import 'package:flutter/material.dart';
 
 abstract class AbcDropDownObject<T> {
@@ -10,23 +12,51 @@ class AbcDropDown<T> extends StatelessWidget {
 
   final List<AbcDropDownObject> items;
   final String label;
-  final void Function (T? value) onSelected;
+  final void Function(T? value) onSelected;
   final T? value;
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<T>(
-      hint: Text(label),
-      items: items.map((e) => DropdownMenuItem<T>(
-                value: e.value,
-                child: Text(e.name),
-              ))
-          .toList(),
-      
-      onChanged: onSelected,
-      value: value,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        DropdownButton<T>(
+          hint: Text(label),
+          items: items
+              .map((e) => DropdownMenuItem<T>(
+                    value: e.value,
+                    child: Text(e.name),
+                  ))
+              .toList(),
+          onChanged: onSelected,
+          value: value,
+          iconEnabledColor: Colors.transparent,
+          borderRadius: BorderRadius.circular(20),
+          underline: const SizedBox.shrink(),
+          selectedItemBuilder: (context) => items
+              .map<Widget>(
+                (e) => Container(
+                  width: context.dimensions.maxHorizontalWidth * 0.8,
+                  padding: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: Theme.of(context).primaryColor,
+                        width: 1,
+                      )),
+                  child: Center(child: Text(e.name)),
+                ),
+              )
+              .toList(),
+        ),
+      ],
     );
   }
-
-  
 }
